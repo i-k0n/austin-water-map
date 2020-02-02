ab8eaa0b7dfc6f9ab0ce11a04340c3050297dce6
i-k0n <mbaker.github@gmail.com>
2020-02-02 16:25:46 -0600

    map component renders

---
+++ src/components/Map.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react'
+import ReactMapGL from 'react-map-gl';
+
+const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_KEY;
+
+const Map = () => {
+    const [viewport, setViewport] = useState({
+        latitude: 30.25844534968869,
+        longitude: -97.80411406405295,
+        zoom: 11,
+        bearing: 0,
+        pitch: 0
+      });
+
+
+    
+    return (
+        <ReactMapGL 
+            {...viewport}
+            width="calc(90vw - 2px)"
+            height="50vh"
+            mapStyle='mapbox://styles/mapbox/outdoors-v9'
+            onViewportChange={setViewport}
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+        />
+    )
+}
+
+export default Map;
